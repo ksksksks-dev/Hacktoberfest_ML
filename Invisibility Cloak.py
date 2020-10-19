b3689f6d371d5b53b4e4ef862ba48fd78cdc74a5
Vinay Kumar Verma <vermavinay982@gmail.com>
2020-10-19 13:58:27 +0530

    Refactored the code, added camera handling
    made the code readable, managed if camera stream broken, releasing the camera after program ends.

--- Invisibility Cloak.py
+++ Invisibility Cloak.py
@@ -2,28 +2,34 @@ import cv2
 import numpy as np
 import time
 
-print("""
-
+message = """
 Harry :  Hey !! Would you like to try my invisibility cloak ??
-
          Its awesome !!
-
         
-         Prepare to get invisible .....................
-    """)
+.................Prepare to get invisible .....................
+"""
+print(message)
 
 
 cap = cv2.VideoCapture(0)
+
+print('Dont Move : Wait 3 seconds the program is loading - Background Capture')
 time.sleep(3)
-background=0
+
+background=None
+
 for i in range(30):
-	ret,background = cap.read()
+	ret, background = cap.read()
 
 background = np.flip(background,axis=1)
 
 while(cap.isOpened()):
 	ret, img = cap.read()
 	
+	if not ret:
+		print('Video Stream Broken')
+		break
+
 	# Flipping the image (Can be uncommented if needed)
 	img = np.flip(img,axis=1)
 	
@@ -34,22 +40,27 @@ while(cap.isOpened()):
 	blurred = cv2.GaussianBlur(hsv, value,0)
 	
 	# Defining lower range for red color detection.
-	lower_red = np.array([0,120,70])
-	upper_red = np.array([10,255,255])
-	mask1 = cv2.inRange(hsv,lower_red,upper_red)
+	lower_red = np.array([0, 120, 70])
+	upper_red = np.array([10, 255, 255])
+	mask1 = cv2.inRange(hsv,lower_red, upper_red)
 	
 	# Defining upper range for red color detection
-	lower_red = np.array([170,120,70])
-	upper_red = np.array([180,255,255])
-	mask2 = cv2.inRange(hsv,lower_red,upper_red)
+	lower_red = np.array([170, 120, 70])
+	upper_red = np.array([180, 255, 255])
+	mask2 = cv2.inRange(hsv,lower_red, upper_red)
 	
 	# Addition of the two masks to generate the final mask.
-	mask = mask1+mask2
+	mask = mask1 + mask2
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5),np.uint8))
 	
 	# Replacing pixels corresponding to cloak with the background pixels.
 	img[np.where(mask==255)] = background[np.where(mask==255)]
+	
 	cv2.imshow('Display',img)
-	k = cv2.waitKey(10)
-	if k == 27:
+	k = cv2.waitKey(1)
+
+	if k == ord('q'):
 		break
+
+cap.release()
+cv2.destroyAllWindows()
